54e7e1f58b2129695a20c53a528d6123307f0c5f
AndMauVe <170448191+AndMauVe@users.noreply.github.com>
2026-03-26 20:45:30 -0500

    config

--- Core/database.py
+++ Core/database.py
@@ -10,7 +10,7 @@ load_dotenv()
 DB_USER = os.getenv("DB_USER", "postgres")
 DB_PASSWORD = os.getenv("DB_PASSWORD", "postgres")
 DB_HOST = os.getenv("DB_HOST", "localhost")
-DB_PORT = os.getenv("DB_PORT", "5432")
+DB_PORT = os.getenv("DB_PORT", "8000")
 DB_NAME = os.getenv("DB_NAME", "DB_M7C3")
 
 DATABASE_URL = os.getenv("DATABASE_URL")
